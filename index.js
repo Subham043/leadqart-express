3a38766adef786a19b7b1fc6eeef7d8675758bcf
ajayjury <ajay@jurysoft.com>
2021-10-16 10:56:01 +0530

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 const helmet = require("helmet");
 const hpp = require('hpp');
+const cors = require('cors');
 const { limiter } = require('./middleware/rate-limiter');
 
 const app = express();
@@ -18,6 +19,20 @@ app.use(hpp());
 app.use(cookieParser())
 //  apply to all requests
 app.use(limiter);
+
+//cors
+// const whitelist = ['http://example1.com', 'http://example2.com']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
+app.use(cors())
 
 //controller-routes
 let authController = require('./controller');
