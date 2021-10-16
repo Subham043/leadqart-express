a4dc7f68eecd67e3539be362fd2eb289aabb154a
ajayjury <ajay@jurysoft.com>
2021-10-16 12:17:59 +0530

    database credentials update

--- model/connection.js
+++ model/connection.js
@@ -6,9 +6,9 @@ const {Sequelize, DataTypes}  = require("sequelize");
 //     logging: false, //sql query logging in console
 //     pool:{max:5,min:0,idle:10000}
 // })
-const sequelize = new Sequelize('db2jg6l5rf2i2r', 'pwkhgbxuhsxxok', 'fb595eb184d77bf050ba080355c39e63791c5b9999ff59a970d96f1a1a756db1',{
-    host:'ec2-3-222-235-188.compute-1.amazonaws.com',
-    dialect: 'postgres',
+const sequelize = new Sequelize('a5inepro_leadqart', 'a5inepro_a5ineprojects9', '%JdE89iD$Jov',{
+    host:'209.99.16.221',
+    dialect: 'mysql',
     logging: false, //sql query logging in console
     pool:{max:5,min:0,idle:10000}
 })
