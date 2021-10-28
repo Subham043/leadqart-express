f78831db0b34f57b4cfd3737e694048521146585
ajayjury <ajay@jurysoft.com>
2021-10-28 14:43:10 +0530

    changed the status of rate limit

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -4,6 +4,7 @@ const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
+    statusCode: 200,
     message:{
         rateLimit:"Too many requests from this IP, please try again after fifteen minutes"
     }
@@ -12,6 +13,7 @@ const limiter = rateLimit({
 const AuthLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 1 hour
     max: 5, // limit each IP to 100 requests per windowMs
+    statusCode: 200,
     message:{
         rateLimit:"Too many request from this IP, please try again after an hour"
     }
