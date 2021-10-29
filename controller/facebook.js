14feb777b5e0fd1685ea3af1d31aa702e99fd8bc
ajayjury <ajay@jurysoft.com>
2021-10-29 14:10:50 +0530

    facebook webhook updated

--- controller/facebook.js
+++ controller/facebook.js
@@ -94,33 +94,31 @@ router.get('/connection/ramya', async (req, res) => {
 })
 
 // Adds support for GET requests to our webhook
-router.get('/webhook', (req, res) => {
+app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
     let VERIFY_TOKEN = "abc123"
       
     // Parse the query params
-    // let mode = req.query['hub.mode'];
+    let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
       
     // Checks if a token and mode is in the query string of the request
-    if (token) {
+    if (mode && token) {
     
       // Checks the mode and token sent is correct
-    //   if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      if (token === VERIFY_TOKEN) {
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
         
         // Responds with the challenge token from the request
         console.log('WEBHOOK_VERIFIED');
-        res.status(200).json({challenge});
+        res.status(200).send(challenge);
       
       } else {
         // Responds with '403 Forbidden' if verify tokens do not match
         res.sendStatus(403);      
       }
     }
-    res.sendStatus(403);
   });
 
 
