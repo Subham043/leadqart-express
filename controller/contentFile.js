0f72ce1a7dacbbd3298bba06075067679663cf2a
Subham043 <70531462+Subham043@users.noreply.github.com>
2021-11-09 17:40:47 +0530

    Update contentFile.js

--- controller/contentFile.js
+++ controller/contentFile.js
@@ -16,12 +16,10 @@ router.post('/create/',
     //custom validations
     body('name').custom(async (value) => textValidation(value, 'name')),
     body('upload').custom(async (value, { req }) => {
-        console.log(req.files.upload.mimetype);
-        console.log(req.files.upload);
         if(!req.files || Object.keys(req.files).length === 0){
             return Promise.reject('Please select a file');
         }
-        if (req.files.upload.mimetype != 'aplication/pdf') {
+        if (req.files.upload.mimetype != 'application/pdf') {
             return Promise.reject('Invalid file type');
         }
         return true;
